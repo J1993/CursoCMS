337c1d5a503f46f13b1cf1475f73405cf031aded
Jordy Lopez <jordizeus93@gmail.com>
2019-04-11 00:36:26 +0100

    Create bd.php

---
+++ config/bd.php
@@ -0,0 +1,12 @@
+<?php
+
+    return array {
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'user ' => '',
+        'password' => '',
+        'database' => '',
+        'charset' => 'utf8'
+    };
+
+?>
